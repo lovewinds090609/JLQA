9c652fdcf7604de3a135ac84c882a9a69507dd44
D0915666 <lovewinds090609@gmail.com>
2024-08-08 20:54:23 +0800

    Q3 done

--- JLQA/Q3/DoorGame.cs
+++ JLQA/Q3/DoorGame.cs
@@ -8,5 +8,51 @@ namespace JLQA.Q3
 {
     public class DoorGame
     {
+        private double _initialScore = 1000;
+        public double result;
+        private int bouns = 0;
+        public char option1 , option2;
+        private bool endGame = false;
+        public void Level_1()
+        {
+            while (true)
+            {
+                switch (option1)
+                {
+                    case 'a':
+                        bouns += 500;
+                        break;
+                    case 'b':
+                        _initialScore /= 2;
+                        option1 = 'x';
+                        continue;
+                    case 'c':
+                        endGame = true;
+                        break;
+                }
+                break;
+            }
+            result = _initialScore + bouns;
+        }
+        public void Level_2()
+        {
+            while (true)
+            {
+                switch (option2)
+                {
+                    case 'a':
+                        bouns += 500;
+                        break;
+                    case 'b':
+                        _initialScore /= 2;
+                        option2 = 'x';
+                        continue;
+                    case 'c':
+                        break;
+                }
+                break;
+            }
+            result = _initialScore + bouns;
+        }
     }
 }
--- JLQA/Q3/DoorGameTest.cs
+++ JLQA/Q3/DoorGameTest.cs
@@ -8,5 +8,122 @@ namespace JLQA.Q3
 {
     public class DoorGameTest
     {
+        DoorGame _game;
+        public DoorGameTest()
+        {
+            _game = new DoorGame();
+        }
+        /// <summary>
+        /// 第一關選a第二關選a
+        /// </summary>
+        [Fact]
+        public void AAndA()
+        {
+            _game.option1 = 'a';
+            _game.Level_1();
+            Assert.Equal(1500,_game.result);
+            _game.option2 = 'a';
+            _game.Level_2();
+            Assert.Equal(2000, _game.result);
+        }
+        /// <summary>
+        /// 第一關選a第二關選b後重選a
+        /// </summary>
+        [Fact]
+        public void AAndBAndA()
+        {
+            _game.option1 = 'a';
+            _game.Level_1();
+            Assert.Equal(1500, _game.result);
+            _game.option2 = 'b';
+            _game.Level_2();
+            Assert.Equal(1000, _game.result);
+            _game.option2 = 'a';
+            _game.Level_2();
+            Assert.Equal(1500, _game.result);
+        }
+        /// <summary>
+        /// 第一關選a第二關選b後重選c
+        /// </summary>
+        [Fact]
+        public void AAndBAndC()
+        {
+            _game.option1 = 'a';
+            _game.Level_1();
+            Assert.Equal(1500, _game.result);
+            _game.option2 = 'b';
+            _game.Level_2();
+            Assert.Equal(1000, _game.result);
+            _game.option2 = 'c';
+            _game.Level_2();
+            Assert.Equal(1000, _game.result);
+        }
+        /// <summary>
+        /// 第一關選b後重選a 第二關選a
+        /// </summary>
+        [Fact]
+        public void BAndAAndA()
+        {
+            _game.option1 = 'b';
+            _game.Level_1();
+            Assert.Equal(500, _game.result);
+            _game.option1 = 'a';
+            _game.Level_1();
+            Assert.Equal(1000, _game.result);
+            _game.option2 = 'a';
+            _game.Level_2();
+            Assert.Equal(1500, _game.result);
+        }
+        /// <summary>
+        /// 第一關選b後重選a 第二關選c
+        /// </summary>
+        [Fact]
+        public void BAndAAndC()
+        {
+            _game.option1 = 'b';
+            _game.Level_1();
+            Assert.Equal(500, _game.result);
+            _game.option1 = 'a';
+            _game.Level_1();
+            Assert.Equal(1000, _game.result);
+            _game.option2 = 'c';
+            _game.Level_2();
+            Assert.Equal(1000, _game.result);
+        }
+        /// <summary>
+        /// 第一關選b第二關選c
+        /// </summary>
+        [Fact]
+        public void BAndC()
+        {
+            _game.option1 = 'b';
+            _game.Level_1();
+            Assert.Equal(500, _game.result);
+            _game.option2 = 'c';
+            _game.Level_2();
+            Assert.Equal(500, _game.result);
+        }
+        /// <summary>
+        /// 驗證選擇b非常多次分數是否大於0
+        /// </summary>
+        [Fact]
+        public void BPowerN()
+        {
+            for (int i = 0; i < 1000; i++) {
+                _game.option1 = 'b';
+                _game.Level_1();
+            }
+            Assert.True(_game.result > 0);
+        }
+        /// <summary>
+        /// 第一關選c
+        /// </summary>
+        [Fact]
+        public void C()
+        {
+            _game.option1 = 'c';
+            _game.Level_1();
+            Assert.Equal(1000, _game.result);
+        }
     }
 }
