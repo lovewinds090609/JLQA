89a4c35a43497f5be55ea7106420b4f9b93e6ce4
D0915666 <lovewinds090609@gmail.com>
2024-08-08 15:58:06 +0800

    加入專案檔案。

---
+++ JLQA/Q1/LoginRewards.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JLQA.Q1
+{
+    public class LoginRewards
+    {
+        private HashSet<int> _loggedInDays = new HashSet<int>();
+        private Dictionary<int, string> _rewards = new Dictionary<int, string>
+        {
+            { 1,"A"},{2,"B"}, {3,"C"}
+        };
+        private List<string> _earnedRewards = new List<string>();
+
+        public void Login(int day)
+        {
+            if (day < 1 || day > 3 || _loggedInDays.Contains(day)) return;
+            _loggedInDays.Add(day);
+            if (_rewards.ContainsKey(day))
+            {
+                _earnedRewards.Add(_rewards[day]);
+            }
+        }
+        public HashSet<int> GetLoggedInDays()
+        {
+            return _loggedInDays;
+        }
+        public List<string> GetEarnedRewards()
+        {
+            return _earnedRewards;
+        }
+    }
+}
---
+++ JLQA/Q1/LoginRewardsTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace JLQA.Q1
+{
+    public class LoginRewardsTest
+    {
+        private readonly LoginRewards _loginRewards;
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public LoginRewardsTest() {
+            _loginRewards = new LoginRewards();
+        }
+        /// <summary>
+        /// 沒有登入任何一天
+        /// </summary>
+        [Fact]
+        public void NoLogin()
+        {
+            Assert.Empty(_loginRewards.GetEarnedRewards());
+        }
+        /// <summary>
+        /// 分別登入第1天 第2天 第3天
+        /// </summary>
+        /// <param name="day"></param>
+        /// <param name="reward"></param>
+        [Theory]
+        [InlineData(1,"A")]
+        [InlineData(2,"B")]
+        [InlineData(3,"C")]
+        public void LoginOneDay(int day , string reward) {
+            _loginRewards.Login(day);
+            Assert.Equal(reward, _loginRewards.GetEarnedRewards()[0]);
+        }
+        /// <summary>
+        /// 登入某2天
+        /// </summary>
+        /// <param name="day1"></param>
+        /// <param name="day2"></param>
+        /// <param name="rewards"></param>
+        [Theory]
+        [InlineData(1, 2, "AB")]
+        [InlineData(1, 3, "AC")]
+        [InlineData(2, 3, "BC")]
+        public void LoginTwoDays(int day1,int day2,string rewards)
+        {
+            _loginRewards.Login(day1);
+            _loginRewards.Login(day2);
+            Assert.Equal(rewards, _loginRewards.GetEarnedRewards()[0]+ _loginRewards.GetEarnedRewards()[1]);
+        }
+        /// <summary>
+        /// 3天都登入
+        /// </summary>
+        [Fact]
+        public void LoginThreeDays()
+        {
+            _loginRewards.Login(1);
+            _loginRewards.Login(2);
+            _loginRewards.Login(3);
+            Assert.Equal("ABC", _loginRewards.GetEarnedRewards()[0]+ _loginRewards.GetEarnedRewards()[1]+ _loginRewards.GetEarnedRewards()[2]);
+        }
+        /// <summary>
+        /// 重複登入
+        /// </summary>
+        /// <param name="day"></param>
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void RepeatLogin(int day)
+        {
+            _loginRewards.Login(day);
+            _loginRewards.Login(day);
+            ///只能有1個獎勵
+            Assert.Single(_loginRewards.GetEarnedRewards());
+        }
+        /// <summary>
+        /// 邊界測試
+        /// </summary>
+        /// <param name="day"></param>
+        [Theory]
+        [InlineData(0)]
+        [InlineData(4)]
+        public void LoginOtherDay(int day) 
+        {
+            _loginRewards.Login(day);
+            Assert.Empty(_loginRewards.GetEarnedRewards());
+        }
+    }
+}
