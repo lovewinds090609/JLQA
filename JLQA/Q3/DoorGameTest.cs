8d392c582a3f02216e3be49a2db0b5a72e55652f
D0915666 <lovewinds090609@gmail.com>
2024-08-08 16:05:18 +0800

    新增題目

---
+++ JLQA/Q3/DoorGameTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JLQA.Q3
+{
+    internal class DoorGameTest
+    {
+    }
+}
---
+++ JLQA/Q4/Grid3x3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JLQA.Q4
+{
+    internal class Grid3x3
+    {
+    }
+}
