1d197385b8bf879a04a621ae60332f9eeccf5419
D0915666 <lovewinds090609@gmail.com>
2024-08-09 13:45:39 +0800

    Q4 done

--- JLQA/Q4/Grid3x3.cs
+++ JLQA/Q4/Grid3x3.cs
@@ -8,6 +8,33 @@ namespace JLQA.Q4
 {
     public class Grid3x3
     {
-
+        private Dictionary<string, int> _score = new Dictionary<string, int>
+        {
+            //1分
+            {"123",1 },{"456",1 },{"789",1 },{"147",1 },{"258",1 },{"369",1 },
+            //3分
+            {"123,456",3 },{"123,789",3 },{"456,789",3 },{"147,258",3 },{"147,369",3 },{"258,369",3 },
+            //5分
+            {"123,456,789",5 },{"147,258,369",5 },
+        };
+        public int Play(string line)
+        {
+            int lastCommaIndex;
+            while (true)
+            {
+                if (_score.ContainsKey(line))
+                {
+                    return _score[line];
+                }
+                else
+                {
+                    lastCommaIndex = line.LastIndexOf(",");
+                    if (lastCommaIndex >= 0)
+                    {
+                        line = line.Remove(lastCommaIndex);
+                    }
+                }
+            }
+        }
     }
 }
--- JLQA/Q4/Grid3x3Test.cs
+++ JLQA/Q4/Grid3x3Test.cs
@@ -4,9 +4,86 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using static System.Formats.Asn1.AsnWriter;
 namespace JLQA.Q4
 {
     public class Grid3x3Test
     {
+        Grid3x3 _grid3X3;
+        private static int totalScore = 0;
+        public Grid3x3Test()
+        {
+            _grid3X3 = new Grid3x3 ();
+        }
+        [Theory]
+        [InlineData("123",1)]
+        [InlineData("456",1)]
+        [InlineData("789",1)]
+        [InlineData("147",1)]
+        [InlineData("258",1)]
+        [InlineData("369",1)]
+        public void OnePoint(string line , int exceptScore)
+        {
+            int score = _grid3X3.Play(line);
+            Assert.Equal(exceptScore, score);
+            totalScore += score;
+        }
+        [Theory]
+        [InlineData("123,456", 3)]
+        [InlineData("123,789", 3)]
+        [InlineData("456,789", 3)]
+        [InlineData("147,258", 3)]
+        [InlineData("147,369", 3)]
+        [InlineData("258,369", 3)]
+        public void TwoPoints(string line, int exceptScore)
+        {
+            int score = _grid3X3.Play(line);
+            Assert.Equal(exceptScore, score);
+            totalScore += score;
+        }
+        [Theory]
+        [InlineData("123,456,789", 5)]
+        [InlineData("147,258,369", 5)]
+        public void ThreePoints(string line, int exceptScore)
+        {
+            int score = _grid3X3.Play(line);
+            Assert.Equal(exceptScore, score);
+            totalScore += score;
+        }
+        [Theory]
+        [InlineData("123,147", 1)]
+        [InlineData("123,258", 1)]
+        [InlineData("123,369", 1)]
+        public void OnePointButRepeatLine(string line, int exceptScore)
+        {
+            int score = _grid3X3.Play(line);
+            Assert.Equal(exceptScore, score);
+        }
+        [Theory]
+        [InlineData("123,456,147", 3)]
+        [InlineData("123,456,258", 3)]
+        [InlineData("123,456,369", 3)]
+        public void TwoPointButRepeatLine(string line, int exceptScore)
+        {
+            int score = _grid3X3.Play(line);
+            Assert.Equal(exceptScore, score);
+        }
+        [Theory]
+        [InlineData("123,456,789,147", 5)]
+        [InlineData("123,456,789,258", 5)]
+        [InlineData("123,456,789,369", 5)]
+        [InlineData("123,456,789,147,258", 5)]
+        [InlineData("123,456,789,147,369", 5)]
+        [InlineData("123,456,789,258,369", 5)]
+        public void ThreePointsButRepeatLine(string line, int exceptScore)
+        {
+            int score = _grid3X3.Play(line);
+            Assert.Equal(exceptScore, score);
+        }
+        [Fact]
+        public void TotalScore()
+        {
+            Assert.Equal(34, totalScore);
+        }
     }
 }
